45a34c0f747f24f27febd6ba2901a94f9c0d6276
jyh7a <jyh7a@naver.com>
2022-03-19 16:57:17 +0900

    Create Project

---
+++ .postcssrc.js
@@ -0,0 +1,19 @@
+// ESM Ecma Script Module
+// COMMON JS => NODE => import(X), require(O)
+// module.exports
+
+//  import autoprefixer from 'autoprefixer'
+const autoprefixer = require("autoprefixer");
+
+// export
+// export{
+//   plugins:{
+//     autoprefixer
+//   }
+// }
+
+module.exports = {
+  plugins:[
+    autoprefixer
+  ]
+}
